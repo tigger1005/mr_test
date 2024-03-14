0145e0170a9d0f4052cbf04d7cec516757caabee
REbrecht <roland.ebrecht@gmail.com>
2024-03-14 13:20:18 +0100

    Combined methodes

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,55 @@
-extern crate rand;
+use rand;
 
-use miller_rabin::is_prime;
-use num_bigint::{BigUint, RandBigInt};
-use rand::rngs::ThreadRng;
+use clap::Parser;
+use std::env;
+
+mod prime;
+use prime::get_prime_numer;
+
 use rayon::prelude::*;
 use std::{io::Write, time::Instant};
-const NUMBER_OF_PRIMES: usize = 50000;
 
-fn get_prime_numer(rng: &mut ThreadRng, k: usize) -> usize {
-    let mut count = 0;
-    let mut pos_prime: BigUint;
+/// Program to simulate fault injections on ARMv8-M processors (e.g. M33)
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Number of threads started in parallel
+    #[arg(short, long, default_value_t = 0)]
+    threads: u16,
 
-    loop {
-        // Get odd random number
-        pos_prime = rng.gen_biguint(1024);
-        pos_prime.set_bit(0, true);
+    /// Investigation methode of Miller Rabin runtime
+    #[arg(short, long, default_value_t = String::from("calls"))]
+    investigation: String,
 
-        count += 1;
-        if is_prime(&pos_prime, k) {
-            break;
-        }
-    }
-    count
+    /// Number of primes to be generated
+    #[arg(short, long, default_value_t = 1000)]
+    number_of_primes: usize,
 }
 
-// fn average(numbers: &Vec<usize>) -> f32 {
-//     numbers.iter().sum::<usize>() as f32 / numbers.len() as f32
-// }
+fn average(numbers: &Vec<usize>) -> f32 {
+    numbers.iter().sum::<usize>() as f32 / numbers.len() as f32
+}
 
 fn main() {
-    println!("Check for prime");
-    println!("Number of primes: {}", NUMBER_OF_PRIMES);
+    // Get parameter from command line
+    let args = Args::parse();
+    // Set parameter from cli
+    env::set_var("RAYON_NUM_THREADS", args.threads.to_string());
 
+    println!("Check for prime (1024 bit) numbers with Miller-Rabin test");
+    println!("Number of primes: {}", args.number_of_primes);
+
+    match args.investigation.as_str() {
+        "calls" => mr_calls_investigation(args.number_of_primes, 1),
+        "time" => mr_time_investigation(args.number_of_primes),
+        _ => {}
+    }
+}
+
+fn mr_time_investigation(n: usize) {
     println!("Miller Rabin test with K=3");
     let now = Instant::now();
-    (0..NUMBER_OF_PRIMES).into_par_iter().for_each(|_| {
+    (0..n).into_par_iter().for_each(|_| {
         let mut rng = rand::thread_rng();
         let _count = get_prime_numer(&mut rng, 3);
         print!(".");
@@ -45,7 +60,7 @@ fn main() {
 
     println!("Miller Rabin test with K=25");
     let now = Instant::now();
-    (0..NUMBER_OF_PRIMES).into_par_iter().for_each(|_| {
+    (0..n).into_par_iter().for_each(|_| {
         let mut rng = rand::thread_rng();
         let _count = get_prime_numer(&mut rng, 25);
         print!(".");
@@ -58,7 +73,24 @@ fn main() {
         "Time div: {:.2?}%",
         (elapsed_25.as_secs_f64() / elapsed_3.as_secs_f64()) * 100.0
     );
+}
 
-    //println!("0x{:#?}", &num_of_counts_per_prime);
-    //println!("Average: {}", average(&num_of_counts_per_prime));
+fn mr_calls_investigation(n: usize, k: usize) {
+    let num_of_counts_per_prime: Vec<usize> = (0..n)
+        .into_par_iter()
+        .map(|_| {
+            let mut rng = rand::thread_rng();
+            let count = get_prime_numer(&mut rng, k);
+            //            println!("Prime number found after {} tries", &count);
+            print!(".");
+            _ = std::io::stdout().flush();
+            count
+        })
+        .collect();
+    println!();
+    //    println!("0x{:#?}", &num_of_counts_per_prime);
+    println!(
+        "Average number of Miller-Rabin calls: {}",
+        average(&num_of_counts_per_prime)
+    );
 }
